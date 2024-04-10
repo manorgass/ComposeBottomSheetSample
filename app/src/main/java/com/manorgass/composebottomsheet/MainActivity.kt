6aaa94a911f6f80870781cd0abd906a9c56dce9a
cardone <manorgass@gmail.com>
2024-04-10 18:11:32 +0900

    inset 설정을 통해 시스템 윈도우와 뷰영역이 겹치지 않도록 처리.

--- app/src/main/java/com/manorgass/composebottomsheet/MainActivity.kt
+++ app/src/main/java/com/manorgass/composebottomsheet/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.manorgass.composebottomsheet.ui.theme.ComposeBottomSheetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             ComposeBottomSheetTheme {
                 // A surface container using the 'background' color from the theme
